40d126ce9c9f9e7afe5918966e2e8c8cc640b1c7
Rocco Punghellini <rocco.punghellini@gmail.com>
2024-07-05 14:01:17 +0200

    test cache in production

--- frontend/components/Grid.vue
+++ frontend/components/Grid.vue
@@ -8,16 +8,16 @@ const props = defineProps({
 });
 
 const hoveredProjectId = ref(null);
-
 </script>
 <template>
     <div class="grid-container w-full p-2 pt-32 md:p-4 md:pt-32">
         <div class="grid gap-1 grid-cols-2 md:grid-cols-3">
             <NuxtLink v-for="project in projects" :key="project._id" @mouseover="hoveredProjectId = project._id"
                 @mouseleave="hoveredProjectId = null" class="grid-item w-fit h-fit hover:rotate-1 hover:bg-yellow-dark transition" :to="`/project/${project.slug}`">
-                <Image class="project-image" :class="{ 'mix-blend-multiply': hoveredProjectId === project._id }"
-                    :src="$urlFor(project.cover).quality(50).format('avif').url()" layout="constrained"
-                    :width="project.coverWidth" :height="project.coverHeight" alt="A lovely bath" />
+                <Image 
+                class="project-image" :class="{ 'mix-blend-multiply': hoveredProjectId === project._id }"
+                :src="$urlFor(project.cover).quality(50).format('avif').url()" layout="constrained"
+                :width="project.coverWidth" :height="project.coverHeight" :alt="project.title" />
                 <div class="md:flex flex-row items-baseline justify-between py-1">
                     <h2 class="typo--m">{{ project.title }}</h2>
                     <span class="typo--m-alt lowercase">{{ project.technique }}</span>
---
+++ frontend/components/PlaceholderImage.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+<template>
+    <div class="placeholder">
+        <div class="gradient element1"></div>
+    </div>
+</template>
+<style scoped>
+.placeholder {
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    position: relative;
+    background-color: var(--color-gray-200);
+}
+.element1 {
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+}
+
+.gradient {
+    animation-duration: 1.8s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-name: placeHolderShimmer;
+    animation-timing-function: linear;
+    background: #f6f6f6;
+    background: linear-gradient(to right, var(--color-grey-100) 10%, #f6f6f6 30%, var(--color-grey-100) 80%);
+    background-size: 1000px 640px;
+
+    position: relative;
+}
+
+@keyframes placeHolderShimmer {
+    0% {
+        background-position: -468px 0;
+    }
+
+    100% {
+        background-position: 468px 0;
+    }
+}
+</style>
